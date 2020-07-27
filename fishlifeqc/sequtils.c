7244c1fd698ccb59a5ed302036df01d8b0d45790
Ulises Rosas <ulisesfrosasp@gmail.com>
2020-07-27 01:05:18 -0400

    split fasta method

--- fishlifeqc/sequtils.c
+++ fishlifeqc/sequtils.c
@@ -4,21 +4,27 @@
 #include <string.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include <math.h>
 
 #define PY_SSIZE_T_CLEAN
 
 // const char* myfile    = "../data/test_hyphen.txt";
 // const char* outmyfile = "not_aln_test_hyphen.txt";
+// const char* myfile    = "../data/noaln_test_hyphen.txt";
 
-// void dealignment(char myarray[], const char* outname);
+// int dealignment(char myarray[], const char* outname);
 // char* readfasta(const char* name);
+// int intlen(int number);
+// int getheaders(char myarray[], int itsize);
+// int get_queries(char myarray[], const char* filename);
 
 // int main(void){
 
 //     char *myresults = readfasta(myfile);
-//     dealignment(myresults, outmyfile); 
+//     get_queries(myresults, myfile);
 
 //     free(myresults);
+//     return 0;
 // }
 
 char* readfasta(const char* name){
@@ -48,7 +54,6 @@ char* readfasta(const char* name){
     string[fsize] = '\0';    
     return string;
 }
-
 
 int dealignment(char myarray[], const char* outname){
 
@@ -88,6 +93,96 @@ int dealignment(char myarray[], const char* outname){
     return 0;
 }
 
+int intlen(int number){
+
+    if(!number){
+        return 1;
+    }
+
+    return floor( log10( abs( number ) ) ) + 1;
+}
+
+int getheaders(char myarray[], int itsize){
+
+    // int itsize = strlen(myarray);
+    char mychar;
+    int headers = 0;
+
+    for (int i = 0; i < itsize; i++)
+    {
+        mychar = myarray[i];
+        if(mychar == '>'){
+            headers++;
+        }        
+    }
+    return headers;
+}
+
+int get_queries(char myarray[], const char* filename){
+
+    int itsize = strlen(myarray);
+    int headers = getheaders(myarray, itsize);
+
+    if (!headers)
+    {
+        return -1;
+    }
+
+    const char* prefix = "_query";
+    const int buffer   = strlen(filename) + strlen(prefix);
+    char mychar2;
+
+    for (int i = 0; i < headers; i++)
+    {
+        int   mystrln = buffer + intlen(i) + 1;
+        char* OUTFILE = malloc(mystrln);
+        snprintf(OUTFILE, mystrln, "%s%s%d", filename, prefix, i);
+
+        FILE *fp      = fopen(OUTFILE, "w");
+        int headcount = 0;
+
+        for (int j = 0; j < itsize; j++)
+        {
+            mychar2 = myarray[j];
+            if(mychar2 == '>')
+            {
+                if (headcount == i)
+                {
+                    while (true)
+                    {
+                        fprintf(fp, "%c", mychar2);
+
+                        j++;
+                        mychar2 = myarray[j];
+                        /*
+                        if j is more than itsize,
+                        it will overindex and return
+                        segmentation fault error
+                        */
+                        if (mychar2 == '>' || j >= itsize)
+                        {
+                            break;
+                        }
+                    }
+                    break;
+                    /*
+                    once matched and extracted all
+                    data and break the whole loop 
+                    */
+                }
+                headcount++;
+                /*
+                count heads until 
+                it matchs with `i` with header
+                count (sequence number)
+                */
+            }
+        }
+        fclose(fp);
+    }
+    return 0;
+}
+
 static PyObject* rm_hyphens(PyObject *self, PyObject *args){
 
     int sts;
@@ -116,12 +211,39 @@ static PyObject* rm_hyphens(PyObject *self, PyObject *args){
     return PyLong_FromLong(sts);
 }
 
+static PyObject* splitfasta(PyObject *self, PyObject *args){
+
+    int sts;
+    const char* input;
+
+    if ( !PyArg_ParseTuple(args, "s", &input) )
+    {
+        PyErr_SetString(PyExc_SystemError, "Argument parsing issues\n");
+        return (PyObject *) NULL;
+    }
+
+    char *myresults = readfasta(input);
+
+    if (!myresults)
+    {
+        PyErr_SetString(PyExc_ValueError, "Issues reading the input file\n");
+        return (PyObject *) NULL;
+    }
+
+    // dealignment(myresults);
+    sts = get_queries(myresults, input);
+    free(myresults);
+    
+    return PyLong_FromLong(sts);
+}
+
 static PyObject* version(PyObject* self){
     return Py_BuildValue("s", "version 0.1");
 }
 
 static PyMethodDef sequtilsmethods[] = {
-    {"rm_hyphens", rm_hyphens, METH_VARARGS, "Remove hyphens from alignment"},
+    {"rm_hyphens", rm_hyphens,   METH_VARARGS, "Remove hyphens from alignment"},
+    {"splitfasta", splitfasta, METH_VARARGS, "Split fasta by sequence into multiple text files"},
     {"version", (PyCFunction)version, METH_NOARGS, "Returns the version."},
     {NULL, NULL, 0, NULL}
 };
