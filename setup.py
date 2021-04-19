58e14d78571e6782562a5aee7f04e2d386284422
Ulises Rosas <ulisesfrosasp@gmail.com>
2021-04-19 06:23:57 -0400

    bump new version

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ with open('README.md') as readme_file:
     readme = readme_file.read()
 
 setup(name="fishlifeqc",
-      version='0.2',
+      version='0.4.5',
       author='Ulises Rosas',
       long_description = readme,
       long_description_content_type='text/markdown',
